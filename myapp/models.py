edee1dfeb8b9d2562a5f1b5cae66d31f4fabd8f8
Estefan Cayuela <135605507+estefancayuela@users.noreply.github.com>
2025-02-15 20:30:57 +0100

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     done = models.BooleanField(default=False)
