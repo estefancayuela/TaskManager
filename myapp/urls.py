c690f1dc6d25aa0d8ab8d3c5f9860f982decf681
Estefan Cayuela <estefancaso@gmail.com>
2025-02-16 20:26:09 +0100

    css and last stetic changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    #Home
+    path('', views.index, name='home'),
+    #PROJECTS
     path('projects/', views.projects, name='all_projects'),
     path('projects/<int:id>', views.project_detail, name='project_details'),
     path('create_project/', views.create_project, name='create_project'),
-    #path('tasks/', views.tasks, name='all_tasks'),
+    path('delete_project/<int:id>', views.delete_project, name='delete_project'),
+    #TASKS
     path('create_task/<int:id>', views.create_task, name='create_task'),
-    path('delete_project/<int:id>/', views.delete_project, name='delete_project'),
-    path('delete_task/<int:id>/', views.delete_task, name='delete_task'),
-    path('done_task/<int:id>/', views.done_task, name='done_task'),
+    path('done_task/<int:id>', views.done_task, name='done_task'),
+    path('delete_task/<int:id>', views.delete_task, name='delete_task'),
+    path('delete_done_tasks/<int:id>', views.delete_done_tasks, name='delete_done_tasks'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, Http404
 from .models import Task, Project
 from .forms import CreateNewTask, CreateNewProject
-
 
 # Create your views here.
 
@@ -15,6 +13,8 @@ def index(request):
 #PROJECTS
 def projects(request):
     project_list=list(Project.objects.values())
+    if project_list == []:
+        return render(request, 'projects/projects.html', {'project_list': project_list, 'message': 'No projects yet'})
     return render (request, 'projects/projects.html', {'project_list': project_list})
 
 def create_project(request):
@@ -27,10 +27,11 @@ def create_project(request):
 def project_detail(request, id):
     if request.method == 'GET':    
         project = get_object_or_404(Project, id=id)
-        task_filtered = Task.objects.filter(project_id=id)
+        task_filtered = Task.objects.filter(project_id=id, done=False)
+        task_done = Task.objects.filter(project_id=id, done=True)
         return render(request, 
                     'projects/project_detail.html', 
-                    {'project': project, 'task_filtered': task_filtered})
+                    {'project': project, 'task_filtered': task_filtered, 'task_done': task_done})
     else:
         return redirect('/create_task/%s' % id)
 
@@ -42,29 +43,33 @@ def delete_project(request, id):
 
 
 #TASKS
-def tasks(request):
-    task_list = Task.objects.all()
-    return render (request, 'tasks/tasks.html', {'task_list': task_list})
-
 def create_task(request, id):
     if request.method == 'GET':
         return render(request, 
                       'tasks/create_task.html', 
                       {'form': CreateNewTask()})
-    else:
+    elif request.method == 'POST':
         Task.objects.create(
             title=request.POST['title'],
             description=request.POST['description'],
             project_id=id,)
         return redirect('/projects/%s' % id)
 
-def delete_task(request, id):
-    TaskToDelete = get_object_or_404(Task, id=id)
-    TaskToDelete.delete()
-    return redirect('/tasks/')
-
 def done_task(request, id):
     task = get_object_or_404(Task, id=id)
-    task.done = True
+    if task.done == True:
+        task.done = False
+    else:
+        task.done = True
     task.save()
     return redirect('/projects/%s' % task.project_id)
+
+def delete_task(request, id):
+    task_projectID = Task.objects.get(id=id).project_id
+    TaskToDelete = get_object_or_404(Task, id=id)
+    TaskToDelete.delete()
+    return redirect('/projects/%s' % task_projectID)
+
+def delete_done_tasks(request, id):
+    Task.objects.filter(project_id=id, done=True).delete()
+    return redirect('/projects/%s' % id)
